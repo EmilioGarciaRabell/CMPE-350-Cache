07a6e6222fd42d21d56e345392cdff6a455dab77
emigar <emiliogarciarabel@gmail.com>
2025-04-14 18:52:33 -0400

    prompts

---
+++ cache.py
@@ -0,0 +1,5 @@
+# prompt user to provide nominal size in Bytes
+nominal_size = int(input("Enter nominal size in Bytes: "))
+
+# prompt user to enter mapping policy
+mapping_policy = input("Enter mapping policy (Direct Mapped or Fully Associative): ").strip().lower()
