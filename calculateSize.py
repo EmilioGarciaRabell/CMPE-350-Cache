c52346a1342ed0a7cc6d71693a8796da29dd55f6
Cole Brindisi <cab9337@g.rit.edu>
2025-04-16 11:26:42 -0400

    Calculations done

--- calculateSize.py
+++ calculateSize.py
@@ -1,16 +1,25 @@
+import math
+
 # Returns integer
 def numBlocks(cacheSize, wordsPerBlock):
-    return 0
+    return cacheSize / (wordsPerBlock * 4.0)
 
+# typeC = 0 for Direct mapped and 1 for N-way
 # Returns integer, -1 if Direct Mapped
-def numSets(cacheSize, wordsPerBlock, N, typeC):
-    return 0
+def numSets(numBlocks, N, typeC):
+    if typeC:
+        return numBlocks / N
+    return -1    
 
+# typeC = 0 for Direct mapped and 1 for N-way
 # Returns tuple [size(tag), size(index), size(offset)]
 def addressType(wordsPerBlock, numBlocks, typeC):
-    return 0
+    indexSize = math.log2(numBlocks)
+    offsetSize = wordsPerBlock * 4
+    tagSize = 32 - indexSize - offsetSize
+
+    return [tagSize, indexSize, offsetSize]
 
 # Returns double
 def realSize(cacheSize, numBlocks, tagSize, statusSize):
-    return 0
-
+    return cacheSize + numBlocks * (tagSize + statusSize) / 8
