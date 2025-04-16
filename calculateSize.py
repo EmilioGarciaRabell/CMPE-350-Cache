f2cf3d7f0127767cb787b7872967901affe37805
Cole Brindisi <cab9337@g.rit.edu>
2025-04-16 11:11:56 -0400

    Comments done

---
+++ calculateSize.py
@@ -0,0 +1,16 @@
+# Returns integer
+def numBlocks(cacheSize, wordsPerBlock):
+    return 0
+
+# Returns integer, -1 if Direct Mapped
+def numSets(cacheSize, wordsPerBlock, N, typeC):
+    return 0
+
+# Returns tuple [size(tag), size(index), size(offset)]
+def addressType(wordsPerBlock, numBlocks, typeC):
+    return 0
+
+# Returns double
+def realSize(cacheSize, numBlocks, tagSize, statusSize):
+    return 0
+
